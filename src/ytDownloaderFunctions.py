37c2640ac3b9ddd744dab798ca0456fde8dc9660
Rafael Molina García <rmolinag2002@gmail.com>
2023-10-16 12:59:07 +0200

    Progressbar functionality
    Now the progressbar shows progress for merging proccess

--- src/ytDownloaderFunctions.py
+++ src/ytDownloaderFunctions.py
@@ -3,13 +3,15 @@ from tkinter import messagebox
 from tkinter import filedialog
 import yt_dlp
 import os
+import subprocess
+import re
 
 
-def download_video(url, format_code,output_folder,progress_bar):
+def download_video(url, format_code, output_folder, progress_bar):
     ydl_opts = {
         'format': format_code,
         'outtmpl': os.path.join(output_folder.get(), '%(title)s.%(ext)s'),
-        'progress_hooks':[progress_hook(progress_bar)]
+        'progress_hooks': [progress_hook(progress_bar)]
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -17,15 +19,22 @@ def download_video(url, format_code,output_folder,progress_bar):
         video_filename = ydl.prepare_filename(result)
         video_ext = result['ext']
         audio_codec = result['acodec']
+        duration = float(result['duration'])
+        fps = float(result['fps'])
 
-    return video_filename, video_ext, audio_codec
+    return video_filename, video_ext, audio_codec, calculate_total_frames(duration, fps)
 
 
-def download_audio(url,output_folder,progress_bar):
+def calculate_total_frames(duration, fps):
+    total_frames = int(duration * fps)
+    return total_frames
+
+
+def download_audio(url, output_folder, progress_bar):
     ydl_opts = {
         'format': 'bestaudio/best',
         'outtmpl': os.path.join(output_folder.get(), '%(title)s_audio.%(ext)s'),
-        'progress_hooks':[progress_hook(progress_bar)]
+        'progress_hooks': [progress_hook(progress_bar)]
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -36,11 +45,43 @@ def download_audio(url,output_folder,progress_bar):
     return audio_filename, audio_ext
 
 
-def merge_audio_video(video_file, audio_file, output_file):
-    command = f'ffmpeg -i "{video_file}" -i "{audio_file}" -c:v copy -c:a aac -strict experimental "{output_file}"'
-    os.system(command)
+def merge_audio_video(video_file, audio_file, output_file, video_frames,progress_bar):
+    cmd = [
+        'ffmpeg',
+        '-i', video_file,
+        '-i', audio_file,
+        '-c:v', 'copy',
+        '-c:a', 'aac',
+        '-strict', 'experimental',
+        output_file
+    ]
 
-def get_available_resolutions(url,resolutions_text):
+    try:
+        # Start the ffmpeg process and capture its output for real time progress bar
+        process = subprocess.Popen(cmd, stderr=subprocess.PIPE, universal_newlines=True)
+
+        # Define a regex pattern to extract progress information
+        progress_pattern = re.compile(r"frame=(\s*\d+)")
+
+        while True:
+            line = process.stderr.readline()
+            if not line:
+                break
+            match = progress_pattern.search(line)
+            if match:
+                frame_number = int(match.group(1))
+                total_frames = video_frames  # You can adjust this based on your video's total frames
+                progress_percent = (frame_number / total_frames) * 100
+                progress_bar['value'] = progress_percent
+
+        process.communicate()  # Wait for the process to finish
+        if process.returncode != 0:
+            print("Error: FFmpeg process returned a non-zero exit code.")
+    except subprocess.CalledProcessError as e:
+        print("Error:", e)
+
+
+def get_available_resolutions(url, resolutions_text):
     try:
         ydl_opts = {
             'listformats': True,
@@ -60,30 +101,30 @@ def get_available_resolutions(url,resolutions_text):
                 acodec = format.get('acodec')
                 size = format.get('filesize')
 
-                if ext not in ('webm','mp4'):
+                if ext not in ('webm', 'mp4'):
                     continue
 
                 if acodec == 'none':
                     acodec = 'Yes'
                 else:
-                    acodec='No'
+                    acodec = 'No'
 
                 size_mb = float(size) / (1024 * 1024)
                 size_display = f'{size_mb:.2f} MB'
 
-                resolutions_text.insert(parent='',index=tk.END,values=(resolution,size_display,ext,acodec,format_code))
+                resolutions_text.insert(parent='', index=tk.END,
+                                        values=(resolution, size_display, ext, acodec, format_code))
 
     except Exception as e:
-        messagebox.showerror("Error",'No URL or not valid')
+        messagebox.showerror("Error", 'No URL or not valid')
+
 
 def progress_hook(progress_bar):
     def update_progress_bar(d):
         if d['status'] == 'downloading':
             progress_percent = float(d['_percent_str'].strip('%'))
-            progress_bar['value']=progress_percent
+            progress_bar['value'] = progress_percent
         else:
-            progress_bar['value']=0
+            progress_bar['value'] = 0
 
     return update_progress_bar
-
-
